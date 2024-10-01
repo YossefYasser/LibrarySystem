1b710da59bc1a655ec0a6924f39076d09521bd49
YossefYasser <yossef.yasser741@gmail.com>
2024-10-01 10:49:56 +0300

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EnozomTask.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options) { }
+    }
+}
